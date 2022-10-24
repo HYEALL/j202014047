217077715c68c1ca1075a1c3fad1cdc7b42e5b87
Kwak, Seoungeun <kwakse0720@gmail.com>
2022-10-24 16:39:26 +0900

    Main.java 수정1

--- src/j202014047/Main.java
+++ src/j202014047/Main.java
@@ -1,5 +1,8 @@
 package j202014047;
 
-public class Main {
-
+public class Main { 
+    public static void main(String[] args) {
+        String s = "hello world";
+        System.out.println(s); 
+    } 
 }
